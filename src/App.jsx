cf637171b94b723914e2739ad08b5e3b2e5e72c7
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-03-09 19:39:50 +0530

    near to finish  section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import Layout from './components/Layout/Layout';
+import About from './pages/About/About';
+import Techstack from './pages/Tech stack/Techstack';
+import Project from './pages/Projects/Project';
+import Education from './pages/Education/Education';
+import Work from './pages/Work/Work';
+import Contact from './pages/Contact/Contact';
+import ScrollToTop from "react-scroll-to-top";
+import { useTheme } from './context/ThemeContext';
+import MobileNav from "./components/MobileNav/MobileNav";
 
 const App = () => {
+  const [theme] = useTheme();
   return (
-    <div>
-      shiva
+    <div id={theme}>
+      <MobileNav />
+      <Layout />
+      <div className='containers'>
+        <About />
+        <Education />
+        <Techstack />
+        <Project />
+        <Work />
+        <Contact />
+      </div>
+      <div className='footer pb-3 ms-3'>
+        <h4 className='text-center'>
+          Made with Shiva❤️ &copy; 2024
+        </h4>
+      </div>
+      <ScrollToTop smooth color='orange' style={{ background: '#1e1e2c', borderRadius: '80px' }} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
